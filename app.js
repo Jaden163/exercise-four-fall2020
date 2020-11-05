bb438e81ee6a6455c62b0455ad1ea516f5fc95c1
JunHao Chen <jaden.chen163@gmail.com>
2020-11-05 18:29:33 -0500

    Server setup

---
+++ app.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const app = express();
+const port = process.env.POST || 4000;
+const indexRoute = require("./routes/index.js");
+
+app.use("/", indexRoute);
+app.listen(port, () => console.log(`Running app at localhost:${port}`));
