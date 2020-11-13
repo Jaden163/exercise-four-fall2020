bc774acaf0e29748b513ae1e1a6191fc985a2d1f
JunHao Chen <jaden.chen163@gmail.com>
2020-11-12 19:23:59 -0500

    Added individual article and post form

--- routes/createBlogPost.js
+++ routes/createBlogPost.js
@@ -1,9 +1,23 @@
 // create blog post
 
 const express = require("express");
-
 const router = express.Router();
 
-router.get("/", (req, res) => res.send("create blog post"));
+// Require firebase
+const firebase = require("firebase");
+// Initialize firestore db
+const db = firebase.firestore();
+// Reference db collection
+const blogPosts = db.collection("blogPost");
+
+const form = `
+<form>
+    <input type="text" name="title" placeholder="Tile of Post" />
+    <input type="text" name="content" placeholder="Content of Post" />
+    <input type="text" name="author" placeholder="Author" />
+    <button type="submit">Submit Post</button>
+</form>`;
+
+router.get("/", (req, res) => res.send(form));
 
 module.exports = router;
--- routes/post.js
+++ routes/post.js
@@ -1,9 +1,35 @@
 // Show single blog post
 
 const express = require("express");
-
 const router = express.Router();
 
-router.get("/", (req, res) => res.send("Show single blog post"));
+// Require firebase
+const firebase = require("firebase");
+// Initialize firestore db
+const db = firebase.firestore();
+// Reference db collection
+const blogPosts = db.collection("blogPost");
+
+router.get("/"),
+  (req, res) => {
+    return res.send("No id provided");
+  };
+
+router.get("/:id", (req, res) => {
+  const queryId = req.params.id;
+  blogPosts
+    .doc(queryId)
+    .get()
+    .then(function (doc) {
+      if (doc.exists) {
+        return res.send(doc.data());
+      } else {
+        return res.send("No such document exists");
+      }
+    })
+    .catch(function (error) {
+      return res.send(error);
+    });
+});
 
 module.exports = router;
