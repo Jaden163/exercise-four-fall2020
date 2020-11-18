d60a7ecfcac3a7810a6883520726553f9ea053f2
JunHao Chen <jaden.chen163@gmail.com>
2020-11-17 21:25:33 -0500

    Finished create post

--- routes/createBlogPost.js
+++ routes/createBlogPost.js
@@ -11,13 +11,34 @@ const db = firebase.firestore();
 const blogPosts = db.collection("blogPost");
 
 const form = `
-<form>
+<form action="/create/submit">
     <input type="text" name="title" placeholder="Tile of Post" />
     <input type="text" name="content" placeholder="Content of Post" />
     <input type="text" name="author" placeholder="Author" />
     <button type="submit">Submit Post</button>
 </form>`;
 
+// Default route serves form
 router.get("/", (req, res) => res.send(form));
+
+//Route for submitting form
+router.get("/submit", (req, res) => {
+  const queryParams = req.query;
+
+  //custom ID
+  const idFromTitle = queryParams.title.replace(/\s+/g, "-").toLowerCase();
+
+  blogPosts
+    // create new posts or update
+    .doc(idFromTitle)
+    .set(queryParams)
+    .then(function (doc) {
+      res.send("Successful Submission");
+    })
+    .catch(function (error) {
+      console.log("error", error);
+      res.send("Failed Submission");
+    });
+});
 
 module.exports = router;
